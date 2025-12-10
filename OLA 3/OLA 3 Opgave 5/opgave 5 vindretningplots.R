2ffdd9475baa0ce544abe97541aef2911be033b0
viggodideriksen-star <viggodideriksen@gmail.com>
2025-12-10 17:20:12 +0100

    Add files via upload

---
+++ OLA 3/OLA 3 Opgave 5/opgave 5 vindretningplots.R
@@ -0,0 +1,57 @@
+#################århus
+dfan <- expand.grid(
+  x = seq(0, pi, pi/12),
+  y = 0
+)
+
+# Tilføj vindretning
+dfan$winddir <- as.numeric(anholt_flip$value[1:nrow(df)])
+
+# ✅ KORREKT omregning fra kompasgrader til matematiske vektorer:
+dfan$u <- -sin(dfan$winddir * pi / 180)
+dfan$v <- -cos(dfan$winddir * pi / 180)
+install.packages("rnaturalearthdata")
+library(sf)
+
+library(rnaturalearth)
+dk <- ne_countries(scale = "medium", country = "Denmark", returnclass = "sf")
+
+ggplot(dfan, aes(x = x, y = 0, u = u, v = v)) +
+  geom_quiver(color = "darkolivegreen4") +
+  coord_equal()+
+  theme_minimal()+
+  labs(title= "Vinden over Anholt bevægede sig i nordvestgående retning", y = "", x = "Dato")+
+  scale_x_continuous(breaks = seq(0,3, length.out = 8), labels = c("21. november", "20. november",
+                                                           "19. november", "18. november", 
+                                                           "17. november", "16. november",
+                                                           "15. november", "14. november"))+
+  scale_y_continuous(labels = NULL)
+
+#############aarhus
+dfan <- expand.grid(
+  x = seq(0, pi, pi/12),
+  y = 0
+)
+
+# Tilføj vindretning
+dfaa$winddir <- as.numeric(aarhus_flip$value[1:nrow(df)])
+
+# ✅ KORREKT omregning fra kompasgrader til matematiske vektorer:
+dfaa$u <- -sin(dfaa$winddir * pi / 180)
+dfaa$v <- -cos(dfaa$winddir * pi / 180)
+install.packages("rnaturalearthdata")
+library(sf)
+
+library(rnaturalearth)
+dk <- ne_countries(scale = "medium", country = "Denmark", returnclass = "sf")
+
+ggplot(dfaa, aes(x = x, y = 0, u = u, v = v)) +
+  geom_quiver(color = "darkolivegreen4") +
+  coord_equal()+
+  theme_minimal()+
+  labs(title= "Vinden over Aarhus bevægede sig i nordvestgående retning", y = "", x = "Dato")+
+  scale_x_continuous(breaks = seq(0,3, length.out = 8), labels = c("21. november", "20. november",
+                                                                   "19. november", "18. november", 
+                                                                   "17. november", "16. november",
+                                                                   "15. november", "14. november"))+
+  scale_y_continuous(labels = NULL)
